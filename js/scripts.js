456d487e67ae582050c50c40afaf5b4f72041399
Albert Byrone <albertbyrone@1677.com>
2019-08-25 14:06:13 -0400

    adds hover effect on the image1

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,11 @@ $(".image").click(function() {
   $(this).children(".hideMe").slideToggle("1500");
   $(this).children(".show").toggle("1500");
 });
+
+$("#work1")
+  .mouseenter(function() {
+    $("#overlay").show("slow");
+  })
+  .mouseleave(function() {
+    $("#overlay").hide("slow");
+  });
