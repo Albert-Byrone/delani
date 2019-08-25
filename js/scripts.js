0ea3f7cdfaa6a77b8e32458cfd3b89e4a16921ed
Albert Byrone <albertbyrone@1677.com>
2019-08-25 14:27:52 -0400

    holds value from the user contact form

--- js/scripts.js
+++ js/scripts.js
@@ -60,3 +60,19 @@ $("#work8")
   .mouseleave(function() {
     $("#overlay8").hide("slow");
   });
+
+
+  $("form#formInput").submit(function(event){
+    let name = $("input#name").val();
+    let email = $("input#email").val();
+    let subject = $("textarea#subject").val();
+
+    if ($("input#name").val() && $("input#email").val()){
+      alert (name + ", we have received your message. Thank you for reaching out to us.");
+    }
+    else {
+      alert("Please enter your name and email!");
+    }
+
+    event.preventDefault();
+  })
